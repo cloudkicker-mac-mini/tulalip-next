11c63aeff59377b257be12a31195fec5c5062b96
Anthony Lane <anthony@local>
2026-08-07 00:47:04 -0700

    Migrate Tulalip Next UI to Metronic

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="tulalip-next-v1.2.0";
-const ASSETS=["./","./index.html","./styles.css","./mobile-fix.css","./events.js","./app.js","./manifest.webmanifest","./assets/icons/icon.svg","./assets/icons/icon-192.png","./assets/icons/icon-512.png","./assets/icons/icon-maskable-512.png"];
+const CACHE="tulalip-next-v1.3.0";
+const ASSETS=["./","./index.html","./styles.css","./mobile-fix.css","./metronic-theme.css","./events.js","./app.js","./manifest.webmanifest","./assets/plugins/global/plugins.bundle.css","./assets/plugins/global/plugins.bundle.js","./assets/css/style.bundle.css","./assets/js/scripts.bundle.js","./assets/icons/icon.svg","./assets/icons/icon-192.png","./assets/icons/icon-512.png","./assets/icons/icon-maskable-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match("./index.html"))));});
